925439dfa44e51551df131ba6dc3352178b7e7b6
M00nray <sergey.rogozha@gmail.com>
2013-09-02 14:23:09 +0400

    Initial Commit

---
+++ NerdFeedTests/NerdFeedTests.h
@@ -0,0 +1,13 @@
+//
+//  NerdFeedTests.h
+//  NerdFeedTests
+//
+//  Created by Admin on 9/2/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NerdFeedTests : SenTestCase
+
+@end
---
+++ NerdFeedTests/NerdFeedTests.m
@@ -0,0 +1,32 @@
+//
+//  NerdFeedTests.m
+//  NerdFeedTests
+//
+//  Created by Admin on 9/2/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import "NerdFeedTests.h"
+
+@implementation NerdFeedTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NerdFeedTests");
+}
+
+@end
